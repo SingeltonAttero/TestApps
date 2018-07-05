3490d3f46a7a52f266239720b6977529973d01ab
yakov <cen7ur1on007@gmail.com>
2018-07-05 22:57:54 +0200

    add calculate fragment

--- app/src/main/java/com/yakov/weber/calculator/CalculateActivity.kt
+++ app/src/main/java/com/yakov/weber/calculator/CalculateActivity.kt
@@ -1,12 +1,18 @@
 package com.yakov.weber.calculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class CalculateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(main_toolbar)
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container_fragment, CalculateFragment())
+                .commit()
     }
 }
