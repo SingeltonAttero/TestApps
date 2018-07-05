6ed8cda6dbf11378e33a696826602f564599ce9f
yakov <cen7ur1on007@gmail.com>
2018-07-05 22:22:24 +0200

    simple calculate use kotlin

---
+++ app/src/main/java/com/yakov/weber/calculator/CalculateActivity.kt
@@ -0,0 +1,12 @@
+package com.yakov.weber.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
